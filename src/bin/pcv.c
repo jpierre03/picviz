89794f6f70319fa1b4fb3017723f76c34f2c26cb
toady <toady@9f3d20f0-5e4f-0410-9e45-c526e915cc34>
2008-06-18 08:20:30 +0000

    debug/render image as parameter
    git-svn-id: https://picviz.svn.sourceforge.net/svnroot/picviz/trunk@17 9f3d20f0-5e4f-0410-9e45-c526e915cc34

---
+++ src/bin/pcv.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <picviz.h>
+
+int main(int argv, char **argc)
+{
+        struct pcimage_t *image;
+
+        if (argv < 2) {
+                fprintf(stderr, "Syntax: %s file.pcv [-debug|-render]\n",argc[0]);
+                exit(1);
+        }
+
+        picviz_init();
+
+        image = (struct pcimage_t *)pcv_parse(argc[1]);
+        if (!image) {
+                fprintf(stderr, "Cannot parse image %s\n", argc[1]);
+                exit(1);
+        }
+
+        if (argv == 2) {
+                image_to_svg(image);
+        } else {
+                if ( ! strcmp(argc[2], "-debug") ) {
+                        picviz_image_debug_printall(image);
+                } else if ( ! strcmp(argc[2], "-render") ) {
+                        picviz_render_image(image);
+                        picviz_image_debug_printall(image);
+                } else {
+                        fprintf(stderr, "Unknown parameter %s\n", argc[2]);
+                }
+        }
+
+        picviz_image_destroy(image);
+
+        return 0;
+}
