9aa017ec03a7bae8ea8087f55ec6a78e08ca8394
toady <toady@9f3d20f0-5e4f-0410-9e45-c526e915cc34>
2008-07-05 06:28:54 +0000

    header for debug
    git-svn-id: https://picviz.svn.sourceforge.net/svnroot/picviz/trunk@33 9f3d20f0-5e4f-0410-9e45-c526e915cc34

---
+++ src/libpicviz/include/debug.h
@@ -0,0 +1,27 @@
+/*
+ * Picviz - Parallel coordinates ploter
+ * Copyright (C) 2008 Sebastien Tricaud <toady@gscore.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define PICVIZ_DEBUG_NOTICE   0x0001
+#define PICVIZ_DEBUG_WARNING  0x0002
+#define PICVIZ_DEBUG_CRITICAL 0x0003
+
+#define PICVIZ_AREA_PLUGIN 0x0001
+#define PICVIZ_AREA_RENDER 0x0002
+#define PICVIZ_AREA_PARSER 0x0004
+
+void picviz_debug(int level, int area, const char *format, ...);
+
