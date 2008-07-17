bc668808c07cbd333d0d46970e8b4f7c69d8bad9
toady <toady@9f3d20f0-5e4f-0410-9e45-c526e915cc34>
2008-07-17 09:43:33 +0000

    copyright for pcv binary; pkgconfig file for libpicviz
    git-svn-id: https://picviz.svn.sourceforge.net/svnroot/picviz/trunk@73 9f3d20f0-5e4f-0410-9e45-c526e915cc34

---
+++ src/libpicviz/picviz.pc.cmake
@@ -0,0 +1,9 @@
+libdir=@SHLIB_DIR@
+includedir=@INCLUDE_DIR@
+
+Name: Picviz
+Description: Parallel Coordinates Plot Library
+Version: 0.1
+Libs: -L${libdir} -lpicviz
+Cflags: -I${includedir}/picviz
+
