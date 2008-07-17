2d9d3f151b9c234d679b19f73bbbdb9cd0dcf30e
toady <toady@9f3d20f0-5e4f-0410-9e45-c526e915cc34>
2008-07-17 20:11:25 +0000

    correct path for current binding sources tree
    git-svn-id: https://picviz.svn.sourceforge.net/svnroot/picviz/trunk@77 9f3d20f0-5e4f-0410-9e45-c526e915cc34

--- src/libpicviz/bindings/python/setup.py
+++ src/libpicviz/bindings/python/setup.py
@@ -5,9 +5,9 @@ from distutils.core import setup, Extension
 modulepcv = Extension('picviz',
                     define_macros = [('MAJOR_VERSION', '0'),
                                      ('MINOR_VERSION', '1')],
-                    include_dirs = ['../include/'],
+                    include_dirs = ['../../include/'],
                     libraries = ['picviz'],
-                    library_dirs = ['../../../build/src/libpicviz/'],
+                    library_dirs = ['../../../../build/src/libpicviz/'],
                     sources = ['py-picviz.c'])
 
 setup (name = 'Picviz',
