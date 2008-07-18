751111696438c72ffb2302c89854aa60c2a805bb
toady <toady@9f3d20f0-5e4f-0410-9e45-c526e915cc34>
2008-07-18 20:06:11 +0000

    Have python bindings with more info from datastructure
    git-svn-id: https://picviz.svn.sourceforge.net/svnroot/picviz/trunk@85 9f3d20f0-5e4f-0410-9e45-c526e915cc34

--- src/libpicviz/bindings/python/py-picviz.c
+++ src/libpicviz/bindings/python/py-picviz.c
@@ -30,15 +30,49 @@ PyObject *pypicviz_version_get(PyObject *self, PyObject *args)
 	return py_result;
 }
 
+int pypicviz_dict_keyval_append_str(PyObject *dict, char *key, char *val)
+{
+        PyObject *value;
+        int ret;
+
+        value = PyString_FromString(val);
+
+        ret = PyDict_SetItemString(dict, key, value);
+        if (ret < 0) {
+                fprintf(stderr, "[PYTHON_BINDINGS]: Can not append value '%s' to '%s'", val, key);
+        }
+
+        return ret;
+}
+
+int pypicviz_dict_keyval_append_long(PyObject *dict, char *key, long val)
+{
+        PyObject *value;
+        int ret;
+
+        value = PyInt_FromLong(val);
+
+        ret = PyDict_SetItemString(dict, key, value);
+        if (ret < 0) {
+                fprintf(stderr, "[PYTHON_BINDINGS]: Can not append value '%li' to '%s'", val, key);
+        }
+
+        return ret;
+}
+
 PyObject *pypicviz_lines_list_build_from_file(PyObject *self, PyObject *args)
 {
+        int ret;
 	char *filename;
 	struct pcimage_t *image;
+	struct axis_t *axis;
         struct line_t *line;
         struct axisplot_t *axisplot;
+        unsigned int ui = 0;
 
-
-	PyObject *main_list = PyList_New(0);
+        PyObject *main_dict = PyDict_New();
+        PyObject *lineslist = PyList_New(0);
+        PyObject *linedata = PyDict_New();
 
 	picviz_init();
 	PyArg_ParseTuple(args, "s", &filename);
@@ -49,17 +83,55 @@ PyObject *pypicviz_lines_list_build_from_file(PyObject *self, PyObject *args)
 		Py_RETURN_NONE;
 	}
 
+        /* Set Image properties */
+        ret = pypicviz_dict_keyval_append_long(main_dict, "height", image->height);
+        if (ret < 0)
+                Py_RETURN_NONE;
+
+        /* Set axes */
+	llist_for_each_entry(axis, &image->axes->list, list) {
+                ui++;
+        }
+        ret = pypicviz_dict_keyval_append_long(main_dict, "axes_number", ui);
+        if (ret < 0)
+                Py_RETURN_NONE;
+
+        /* Set Lines plots and properties */
 	llist_for_each_entry(line, &image->lines->list, list) {
-		PyObject *axisplot_list = PyList_New(0);
+                PyObject *axisplotlist = PyList_New(0);
+                ret = pypicviz_dict_keyval_append_long(linedata, "id", line->id);
+                if (ret < 0) {
+                        Py_RETURN_NONE;
+                }
+                ret = pypicviz_dict_keyval_append_str(linedata, "color", line->props->color);
+                if (ret < 0) {
+                        Py_RETURN_NONE;
+                }
+                /* Set Line plots */
 		llist_for_each_entry(axisplot, &line->axisplot->list, list) {
-			PyList_Append(axisplot_list, PyInt_FromLong(axisplot->y));
+                        PyObject *plotsdata = PyDict_New();
+                        ret = pypicviz_dict_keyval_append_str(plotsdata, "strval", axisplot->strval);
+                        if (ret < 0) {
+                                Py_RETURN_NONE;
+                        }
+                        ret = pypicviz_dict_keyval_append_long(plotsdata, "y", axisplot->y);
+                        if (ret < 0) {
+                                Py_RETURN_NONE;
+                        }
+                        ret = pypicviz_dict_keyval_append_long(plotsdata, "axis_id", axisplot->axis_id);
+                        if (ret < 0) {
+                                Py_RETURN_NONE;
+                        }
+			PyList_Append(axisplotlist, plotsdata);
+
 		}
-		PyList_Append(main_list, axisplot_list);
+		PyList_Append(lineslist, axisplotlist);
 	}
+        ret = PyDict_SetItemString(main_dict, "lines", lineslist);
 
 	picviz_image_destroy(image);
 
-	return main_list;
+	return main_dict;
 }
 
 
@@ -70,7 +142,7 @@ static PyMethodDef picvizMethods[] =
 {
     { "Version", pypicviz_version_get, METH_VARARGS,
       "Returns the Picviz version"},
-    { "ParseLines", pypicviz_lines_list_build_from_file, METH_VARARGS,
+    { "ParseImage", pypicviz_lines_list_build_from_file, METH_VARARGS,
       "Parse lines from the file"},
 
     {NULL,NULL}
